a58f00652d861562c43216d22da3675111c218fa
CJ <Cjowilson18@gmail.com>
2023-11-15 12:47:21 -0500

    commit all

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,13 +2,13 @@ class JobsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        @job = Job.new(job_params)
+        @job = Job.create(job_params)
         render 'jobs/create'
     end
 
     def byId
         @job= Job.find_by(id: params[:id])
     end
-    
+
 
 end
